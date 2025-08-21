7cdb2a98aa689dcbf1928b360132e521621ab915
Pedro Ricardo <pedro-ricardo.belo@hotmail.com>
2025-08-21 15:31:02 -0300

    atualização 1 utilizando uma classe no cypress para reutilizar codigo

--- cypress/e2e/3-Loja-Ebac/Produtos.cy.js
+++ cypress/e2e/3-Loja-Ebac/Produtos.cy.js
@@ -1,18 +1,31 @@
 ///<reference types="cypress"/>
+import produtosPage from "../../support/page-objets/produtos.page";
 
 describe('Fucionalidade: Produtos', () => {
 
+
     beforeEach(() => {
-        cy.visit('produtos')
+        produtosPage.visitarUrl()
     });
 
-    it('Deve Selecionar um Produto da Lista', () => {
-        cy.get(' .product-block >')
-            //  cy.get('.products > .row')
-            .first()
-            .click()
 
+    it('Deve Selecionar um Produto da Lista', () => {
+        produtosPage.buscarProdutoLista('Aero Daily Fitness Tee')
         cy.get('#tab-title-description > a').should('contain', 'Descrição')
+    });
+
+    it.only('Deve buscar um produto com sucesso', () => {
+        let produto = 'Argus All-Weather Tank'
+        produtosPage.buscarProduto(produto)
+        cy.get('.product_title').should('contain' , produto)
+    });
+
+    it('Deve visitar a página de um produto', () => {
+
+    });
+
+    it('Deve adicionar produto ao carrinho', () => {
+
     });
 
 });
