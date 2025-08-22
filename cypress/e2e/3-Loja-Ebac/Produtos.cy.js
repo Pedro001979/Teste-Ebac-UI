a50d0ebde9eb640648069216dff1b507d7ca2735
Pedro Ricardo <pedro-ricardo.belo@hotmail.com>
2025-08-22 12:31:11 -0300

    atualização do projeto de estudos utilizando busca atraves de massa de dados em lista

--- cypress/e2e/3-Loja-Ebac/Produtos.cy.js
+++ cypress/e2e/3-Loja-Ebac/Produtos.cy.js
@@ -14,18 +14,33 @@ describe('Fucionalidade: Produtos', () => {
         cy.get('#tab-title-description > a').should('contain', 'Descrição')
     });
 
-    it.only('Deve buscar um produto com sucesso', () => {
+    it('Deve buscar um produto com sucesso', () => {
         let produto = 'Argus All-Weather Tank'
         produtosPage.buscarProduto(produto)
         cy.get('.product_title').should('contain' , produto)
     });
 
     it('Deve visitar a página de um produto', () => {
-
+        produtosPage.visitarProduto('Argus All Weather Tank')
+         cy.get('.product_title').should('contain' , 'Argus All-Weather Tank')
     });
 
     it('Deve adicionar produto ao carrinho', () => {
+        let qtd = 7
+        produtosPage.buscarProduto('Aether Gym Pant')
+        produtosPage.addProdutoCarrinho(33 , 'Blue' , qtd)
 
+        cy.get('.woocommerce-message').should('exist')
     });
 
+     it.only('Deve adicionar produto ao carrinho - buscando na massa de dados', () => {
+       cy.fixture('produtos').then(dados => {
+            produtosPage.buscarProduto(dados[2].nomeProduto)
+            produtosPage.addProdutoCarrinho(
+                 dados[2].tamanho ,
+                 dados[2].cor , 
+                 dados[2].quantidade)
+            cy.get('.woocommerce-message').should('contain' , dados[2].nomeProduto)
+        });
+    });
 });
